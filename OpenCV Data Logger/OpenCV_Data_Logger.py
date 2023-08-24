de713fc4fa61fd75eff5ffc4c6ab017658b2ea3d
Brandon Berdan <brandon.berdan@ucalgary.ca>
2023-08-23 18:06:11 -0600

    Added imutils, digit dictionaru

--- OpenCV Data Logger/OpenCV_Data_Logger.py
+++ OpenCV Data Logger/OpenCV_Data_Logger.py
@@ -1,9 +1,16 @@
 import cv2 as cv
+import imutils as imu
+import imutils.perspective as imper
+
+Digits = {
+    (1,1,1,0,1,1,1) : 0,
+    (0,0,1,0,0,1,0) : 1,
+    }
 
 def canny_edge_detection(frame):
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     blur = cv.GaussianBlur(src=gray, ksize=(3,5), sigmaX=0.5)
-    edge = cv.Canny(blur, 80, 145)
+    edge = cv.Canny(blur, 70, 135)
     
     return blur, edge
 
@@ -18,8 +25,8 @@ def main():
     
         blur, edge = canny_edge_detection(frame)
 
-        #cv.imshow('original', frame)
-        cv.imshow('blur', blur)
+        cv.imshow('original', frame)
+        #cv.imshow('blur', blur)
         cv.imshow('edge', edge)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
